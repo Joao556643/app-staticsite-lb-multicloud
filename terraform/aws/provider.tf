5ebb353e8b3ae72ad2f4b34eed19437b2d60db16
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-16 22:55:52 -0300

    hehe

--- terraform/aws/provider.tf
+++ terraform/aws/provider.tf
@@ -6,9 +6,9 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "staticsitelbtfkledson"
+    bucket         = "staticsitelbmulticloudtfjoao"
     key            = "terraform.tfstate"
-    dynamodb_table = "staticsitelbtfkledson"
+    dynamodb_table = "staticsitelbmulticloudtfjoao"
     region         = "us-east-1"
   }
 }
